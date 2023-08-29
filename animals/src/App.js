a9ea6458701e9ac29e2a0db49d67c6a546eccd27
DhruvKauts2 <65485245+DhruvKauts2@users.noreply.github.com>
2023-08-29 22:23:23 +0530

    animals app complete

---
+++ animals/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import './App.css'
+function getRandomAnimal(){
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)] 
+}
+
+console.log(getRandomAnimal());
+
+function App(){
+    const [animals, setAnimals] = useState([]);
+
+
+
+    //event handler
+    const handleClick = () => {
+
+        setAnimals([...animals, getRandomAnimal()])
+    }
+
+    const renderAnimals = animals.map((animal, index) => {
+        return <AnimalShow type = {animal} key = {index}/>
+    })
+
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add animal</button>
+            <div className="animal-list">
+                {renderAnimals}
+            </div>
+        </div>
+        
+    );
+}
+
+export default App;
