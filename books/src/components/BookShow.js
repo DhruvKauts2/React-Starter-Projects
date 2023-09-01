6aa94f9864fb51ff489295bcceb4d5d83a52a299
DhruvKauts2 <65485245+DhruvKauts2@users.noreply.github.com>
2023-09-01 16:53:05 +0530

    Child to parent state management established, major components defined

---
+++ books/src/components/BookShow.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit book={book} />;
+  }
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+  return (
+    <div className="book-show">
+      <div>
+        {content}
+        <div />
+        <div className="actions">
+          <button className="edit" onClick={handleEditClick}>
+            Edit
+          </button>
+          <button className=" delete" onClick={handleDeleteClick}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookShow;
