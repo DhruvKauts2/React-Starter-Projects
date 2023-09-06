ba5b0115eeeee0703c66c1e4d59c44d755a7ebec
DhruvKauts2 <65485245+DhruvKauts2@users.noreply.github.com>
2023-09-06 19:33:22 +0530

    installed prop-types

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button>A</Button>
+      </div>
+      <div>
+        <Button>B</Button>
+      </div>
+      <div>
+        <Button>C</Button>
+      </div>
+      <div>
+        <Button>D</Button>
+      </div>
+      <div>
+        <Button>E</Button>
+      </div>
     </div>
   );
 }
