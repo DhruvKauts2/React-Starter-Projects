e6b6d9db051208a20ff199889ba288c4bbeacf70
DhruvKauts2 <65485245+DhruvKauts2@users.noreply.github.com>
2023-09-08 16:23:51 +0530

    tailwind css installed

---
+++ comps/src/App.js
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Aaa</Button>
+      </div>
+      <div>
+        <Button>Bbb</Button>
+      </div>
+      <div>
+        <Button>Ccc</Button>
+      </div>
+      <div>
+        <Button>Ddd</Button>
+      </div>
+      <div>
+        <Button>Eee</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
